126b9f9d5bc9afad204d77d01fe11f33ab392e9e
Kyle Givler <xvegan88x@gmail.com>
2022-03-13 01:21:57 -0400

    Messing around, expand add form, use some jQuery

--- RecipeWebsite/RecipeWebsite/Models/Recipe.cs
+++ RecipeWebsite/RecipeWebsite/Models/Recipe.cs
@@ -1,12 +1,19 @@
-﻿namespace RecipeWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeWebsite.Models
 {
     public class Recipe
     {
-        public string Name { get; set; }
-        public List<string> IngredientList { get; set; }
-        public int Servings { get; set; }
-        public string  Description { get; set; }
-
+        [Key]
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Ingredients")]
+        public List<string> IngredientList { get; set; }
+
+        public int Servings { get; set; }
     }
 }
